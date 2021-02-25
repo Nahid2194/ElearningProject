93d40d6ed3227c71d8bc6a420ab83e13bef345aa
Nahid2194 <nahidjc80@gmail.com>
2021-02-25 22:41:23 +0600

    TeacherEditForm  adding

--- Login_App/forms.py
+++ Login_App/forms.py
@@ -28,3 +28,9 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+
+class TeacherEditForm(forms.ModelForm):
+    class Meta:
+        model = Teacher
+        exclude = ('user')

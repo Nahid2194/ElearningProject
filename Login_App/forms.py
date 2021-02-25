7e235f8949cbd3b5e17579ae24b5f578064b93d5
Nahid2194 <nahidjc80@gmail.com>
2021-02-25 22:40:03 +0600

    SignUpForm  adding

--- Login_App/forms.py
+++ Login_App/forms.py
@@ -13,3 +13,18 @@ class LoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ('username', 'password')
+
+
+class SignupForm(UserCreationForm):
+    username = forms.CharField(required=True, label="Username", widget=forms.TextInput(
+        attrs={'placeholder': 'username'}))
+    email = forms.EmailField(required=True, label="Email", widget=forms.EmailInput(
+        attrs={'placeholder': 'Enter your Email'}))
+    password1 = forms.CharField(required=True, label="Password", widget=forms.PasswordInput(
+        attrs={'placeholder': 'password'}))
+    password2 = forms.CharField(required=True, label="Password", widget=forms.PasswordInput(
+        attrs={'placeholder': 'password'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')

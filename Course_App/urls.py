6a7f2b471df750a1070e2ea96ffd11a89b12d866
Nahid2194 <nahidjc80@gmail.com>
2021-02-27 00:40:08 +0600

    course details show and comment

--- Course_App/urls.py
+++ Course_App/urls.py
@@ -3,5 +3,9 @@ from django.urls import path, include
 from Course_App import views
 app_name = 'Course_App'
 urlpatterns = [
-    path('create_course/', views.create_course, name='create_course')
+    path('course-details/<slug:slug>/',
+         views.courseDetails, name='course_details'),
+    path('create_course/', views.create_course, name='create_course'),
+    path('question/<pk>', views.question, name='question')
+
 ]
--- Course_App/views.py
+++ Course_App/views.py
@@ -31,3 +31,23 @@ def create_course(request):
             course_obj.save()
             return HttpResponseRedirect(reverse('home'))
     return render(request, 'Course_App/create_course.html', context={'form': form})
+
+
+@login_required
+def courseDetails(request, slug):
+    course = Course.objects.get(slug=slug)
+    form = QuestionForm()
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.user = request.user.student_profile
+            question.course = course
+            question.save()
+            return HttpResponseRedirect(reverse('Course_App:course_details', kwargs={'slug': slug}))
+    return render(request, 'Course_App/course_details.html', context={'course': course, 'form': form})
+
+
+@login_required
+def question(request, pk):
+    return render(request, 'Course_App/question.html', context={})
